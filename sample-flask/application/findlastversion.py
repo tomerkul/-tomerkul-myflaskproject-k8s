864f5eafb6a2f0eb0d3ce3bd20d3501598ef6d60
Tomer_Kulish <91198141+tomerkul@users.noreply.github.com>
2023-07-19 13:42:18 +0300

    Update findlastversion.py

--- sample-flask/application/findlastversion.py
+++ sample-flask/application/findlastversion.py
@@ -3,7 +3,11 @@ import docker
 client = docker.from_env()
 images = client.images.list()
 
-existing_versions = [float(image.tags[0].split(":")[1]) for image in images if image.tags and image.tags[0].startswith("tomerkul/myflask:")]
+existing_versions = []
+for image in images:
+    if image.tags and image.tags[0].startswith("tomerkul/flask:") and image.tags[0] != "tomerkul/myflask:latest":
+        version_str = image.tags[0].split(":")[1]
+        existing_versions.append(float(version_str))
 
 if existing_versions:
     latest_version = max(existing_versions)
@@ -29,3 +33,4 @@ image_to_tag = client.images.get(image_name)
 image_to_tag.tag(repository="tomerkul/myflask", tag=latest_tag)
 client.images.push(repository="tomerkul/myflask", tag=latest_tag)
 print(f"Successfully pushed image: {latest_image_name}")
+
