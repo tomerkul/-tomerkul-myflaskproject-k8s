7f100e8410000e30c4d3112de21a76cb6d443120
Tomer_Kulish <91198141+tomerkul@users.noreply.github.com>
2023-07-31 14:28:39 +0300

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "us-east-2"
 }
 
+data "aws_security_group" "existing" {
+  name = "main"
+}
 
 resource "aws_instance" "example" {
   ami           = "ami-02a89066c48741345"
   instance_type = "t2.micro"
   key_name      = "tst"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [data.aws_security_group.existing.id]
+
+  associate_public_ip_address = true
 
   tags = {
     Name = "Example Instance"
   }
 
   connection {
-      type        = "ssh"
-      user        = "ec2-user" 
-      private_key = file("~/.ssh/tst.pem")
-      host        = self.public_ip  # The public IP of the EC2 instance
-    }
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/tst.pem")
+    host        = self.public_ip
+  }
+
+  provisioner "file" {
+    source      = "docker-compose.yaml"  # Relative path to docker-compose.yaml
+    destination = "/home/ec2-user/docker-compose.yaml"
+  }
+
+  provisioner "file" {
+    source      = "setup.sh"
+    destination = "/tmp/setup.sh"
+  }
 
   provisioner "remote-exec" {
-     inline = [
-       "sudo yum update -y",
-       "sudo yum install -y nginx",
-       "sudo systemctl start nginx",
-       "sudo systemctl enable nginx",
-     ]
+    inline = [
+      "chmod +x /tmp/setup.sh",
+      "sudo /tmp/setup.sh",
+    ]
   }
 
-}
-
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  provisioner "file" {
+    source      = "flask_run.sh"
+    destination = "/tmp/flask_run.sh"
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"  
-    cidr_blocks     = ["10.0.0.0/16"]  
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/flask_run.sh",
+      "sudo /tmp/flask_run.sh",
+    ]
   }
 }
