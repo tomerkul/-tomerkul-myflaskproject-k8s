6947f1b6d7b21f85b9583170039d23e179865793
Tomer_Kulish <91198141+tomerkul@users.noreply.github.com>
2023-08-12 13:54:12 +0300

    Update tests.sh

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 set -ex
-
-#test1
-curl http://44.202.63.7:5000
+EXTERNAL_IP=$(kubectl get service flask-service -o-jsonpath='{.status.loadBalancer.ingress[0].ip}')
+http_response=$(curl -s -o /dev/null -w "%{http_code}" ${EXTERNAL_IP}:5000)
+if [[ $http_response == 200 ]]; then
+    echo "Flask app returned a 200 status code. Test passed!"
+else
+    echo "Flask app returned a non-200 status code: $http_response. Test failed!"
+    exit 1
+fi
